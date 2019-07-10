454cfc929be64bed03e4e02cff4a399b7886e907
Rick Lan <ricklan@gmail.com>
2019-07-10 11:25:44 +1000

    加入關閉記錄選項

--- selfdrive/dragonpilot/dragonconf/__init__.py
+++ selfdrive/dragonpilot/dragonconf/__init__.py
@@ -10,40 +10,10 @@ default_conf = {
   'DragonDisableDriverSafetyCheck': '0',
   'DragonAutoShutdownAt': '30', # in minute
   'DragonTempDisableSteerOnSignal': '0',
+  'DragonDisableLogger': '0',
 }
 
-# def write_json_config(config):
-#   with open(file, 'w') as f:
-#     json.dump(config, f, indent=2, sort_keys=True)
-#   os.chmod(file, 0644)
-
 def dragonpilot_set_params(params):
-  # # create new json file
-  # if not os.path.isfile(file):
-  #   write_json_config(default_conf)
-  #   config = default_conf
-  # else:
-  #   # load from json
-  #   with open(file, 'r') as f:
-  #     config = json.load(f)
-  #
-  #   json_update_needed = False
-  #   # add new keys
-  #   for key, val in default_conf.items():
-  #     if key not in config:
-  #       json_update_needed = True
-  #       config[key] = val
-  #
-  #   # remove invalid keys
-  #   for key, val in config.items():
-  #     if key not in default_conf:
-  #       json_update_needed = True
-  #       config.pop(key, None)
-  #
-  #   # write to json if update needed
-  #   if json_update_needed:
-  #     write_json_config(config)
-
   # set params
   for key, val in default_conf.items():
     if params.get(key) is None:
