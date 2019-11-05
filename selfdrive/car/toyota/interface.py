3c0be0b4cd4f855e9c5b727bc46ec8c0edf78d95
dragonpilot <dgnpilot@gmail.com>
2019-11-05 12:08:24 +1000

    add 2017 Lexus GS450H

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -228,11 +228,21 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.LEXUS_ISH:
       stop_and_go = True # set to true because it's a hybrid
-      ret.safetyParam = 66
+      ret.safetyParam = 77
       ret.wheelbase = 2.79908
       ret.steerRatio = 13.3
       tire_stiffness_factor = 0.444
       ret.mass = 3736.8 * CV.LB_TO_KG + STD_CARGO_KG
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]
+      ret.lateralTuning.pid.kf = 0.00006
+
+    elif candidate == CAR.LEXUS_GSH:
+      stop_and_go = True # set to true because it's a hybrid
+      ret.safetyParam = 77
+      ret.wheelbase = 2.84988
+      ret.steerRatio = 13.3
+      tire_stiffness_factor = 0.444
+      ret.mass = 4112 * CV.LB_TO_KG + STD_CARGO_KG
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]
       ret.lateralTuning.pid.kf = 0.00006
 
