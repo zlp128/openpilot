73d8a0ca40ebb91906b76dccdc72a29f78d03060
dragonpilot <dgnpilot@gmail.com>
2019-12-17 11:01:48 +1000

    auto lane change send alerts

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -51,7 +51,10 @@ def add_lane_change_event(events, path_plan):
         event_name = 'preAutoLaneChangeRight'
       events.append(create_event(event_name, [ET.WARNING]))
   elif path_plan.laneChangeState in [LaneChangeState.laneChangeStarting, LaneChangeState.laneChangeFinishing]:
-      events.append(create_event('laneChange', [ET.WARNING]))
+      event_name = 'laneChange'
+      if path_plan.autoLCAllowed:
+          event_name = 'autoLaneChange'
+      events.append(create_event(event_name, [ET.WARNING]))
 
 
 def isActive(state):
