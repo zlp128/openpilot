6a627cee166638b886dffc78912b610368fd29fb
dragonpilot <dgnpilot@gmail.com>
2020-03-13 12:38:58 +1000

    disable time validation

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -286,15 +286,16 @@ def thermald_thread():
     # **** starting logic ****
 
     # Check for last update time and display alerts if needed
-    now = datetime.datetime.now()
-
-    # show invalid date/time alert
-    time_valid = now.year >= 2019
-    if time_valid and not time_valid_prev:
-      params.delete("Offroad_InvalidTime")
-    if not time_valid and time_valid_prev:
-      params.put("Offroad_InvalidTime", json.dumps(OFFROAD_ALERTS["Offroad_InvalidTime"]))
-    time_valid_prev = time_valid
+    # now = datetime.datetime.now()
+    #
+    # # show invalid date/time alert
+    # time_valid = now.year >= 2019
+    # if time_valid and not time_valid_prev:
+    #   params.delete("Offroad_InvalidTime")
+    # if not time_valid and time_valid_prev:
+    #   params.put("Offroad_InvalidTime", json.dumps(OFFROAD_ALERTS["Offroad_InvalidTime"]))
+    # time_valid_prev = time_valid
+    time_valid = True
 
     # Show update prompt
     # try:
